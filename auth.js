549bc588cf93805f8f99d264492fcdb052d986b1
el-joker-f <60827299+el-joker-f@users.noreply.github.com>
2026-08-23 13:26:07 +0300

    Fix Firebase app id while loading coupon patches

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ import { getAuth, onAuthStateChanged, signOut, setPersistence, browserLocalPersi
 import { getFirestore, doc, getDoc, getDocs, collection, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
 export const SUPER_ADMIN_EMAIL = "fmdyh6636@gmail.com";
-export const firebaseConfig = {apiKey:"AIzaSyBN-MtgWLi0lwBPTzEPYrpwxNe6BCn_3KQ",authDomain:"mandoub-dv.firebaseapp.com",projectId:"mandoub-dv",storageBucket:"mandoub-dv.firebasestorage.app",messagingSenderId:"311140400335",appId:"1:140400335:web:db1987c53259c53594bba",measurementId:"G-7V1LJQYYXD"};
+export const firebaseConfig = {apiKey:"AIzaSyBN-MtgWLi0lwBPTzEPYrpwxNe6BCn_3KQ",authDomain:"mandoub-dv.firebaseapp.com",projectId:"mandoub-dv",storageBucket:"mandoub-dv.firebasestorage.app",messagingSenderId:"311140400335",appId:"1:311140400335:web:db1987c53259c53594bba",measurementId:"G-7V1LJQYYXD"};
 const app=getApps().length?getApps()[0]:initializeApp(firebaseConfig);
 export const auth=getAuth(app);
 export const authReady=setPersistence(auth,browserLocalPersistence).catch(e=>{console.error("Auth persistence error:",e);return null;});
